cdbe836c7609874eeccf932bc999d10499db8b9b
Joven Tan <jovenntann@gmail.com>
2024-08-03 15:43:27 +0800

    feat(login): Add AuthenticationTitle component with styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,47 @@
-'use client';
+import {
+    TextInput,
+    PasswordInput,
+    Checkbox,
+    Anchor,
+    Paper,
+    Title,
+    Text,
+    Container,
+    Group,
+    Button,
+  } from '@mantine/core';
+import classes from './AuthenticationTitle.module.css';
 
-import Image from 'next/image';
-import { AspectRatio } from '@mantine/core';
+export function AuthenticationTitle() {
+    return (
+        <Container size={420} my={40}>
+        <Title ta="center" className={classes.title}>
+            Welcome back!
+        </Title>
+        <Text c="dimmed" size="sm" ta="center" mt={5}>
+            Do not have an account yet?{' '}
+            <Anchor size="sm" component="button">
+            Create account
+            </Anchor>
+        </Text>
 
-function Demo() {
-  return (
-    <AspectRatio ratio={1080 / 720} maw={300} mx="auto">
-      <Image
-        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-5.png"
-        alt="Panda"
-        layout="fill"
-        objectFit="cover"
-      />
-    </AspectRatio>
-  );
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+            <TextInput label="Email" placeholder="you@mantine.dev" required />
+            <PasswordInput label="Password" placeholder="Your password" required mt="md" />
+            <Group justify="space-between" mt="lg">
+            <Checkbox label="Remember me" />
+            <Anchor component="button" size="sm">
+                Forgot password?
+            </Anchor>
+            </Group>
+            <Button fullWidth mt="xl">
+            Sign in
+            </Button>
+        </Paper>
+        </Container>
+    );
 }
 
-export default Demo;
+export default function LoginPage() {
+  return <AuthenticationTitle />;
+}
